7175788542b64a9976fdd37522563fd7386be0f5
jgore43 <jgore43@gmail.com>
2018-01-26 18:10:00 -0500

    Initial Commit

---
+++ config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld_topdown_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1517007983,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "Laggy Cow Server",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "myworld_topdown_t3",
+      "tileSize": 288,
+      "world": "myworld",
+      "worldName": "myworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld_topdown_day"
+  ],
+  "tileSetGroups": {
+    "myworld_topdown_t3": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 3
+    }
+  }
+};
